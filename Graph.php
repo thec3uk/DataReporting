e13e535f46623b0efd8b83e910fdf5c5f0b49f52
Mia Axtell <mia.axtell@outlook.com>
2020-03-06 18:18:30 +0000

    Trying to configure a graph

--- Graph.php
+++ Graph.php
@@ -5,105 +5,7 @@
   $graph = $_GET['graph'];
 
   $title = $ministry . " " . $graph . " Graph (" . $session .")";
-
-  $baseURL = 'https://api.churchsuite.co.uk/v1/children/group/';
-
-  $x_account = 'thec3';
-  $x_application = 'C3-DataReporting';
-  $x_auth = 'keyyzyy5l9uqicjacdtm';
-
-  //Setting API Header
-  $api_headers = array(
-  	'Accept: application/json',
-  	'Content-Type: application/json',
-  	'X-Account: '.$x_account,
-  	'X-Auth: '.$x_auth,
-  	'X-Application: '.$x_application,
-  );
-
-  //If statement to extract the right data and then format it into something useable
-  //Planning on tidying this up later - for now this is how my brain works
-  if ($ministry == "Kids Church") {
-  	$WtP_URL = $baseURL . "2/";
-  	$PtR_URL = $baseURL . "11/";
-  	$Y1t2_URL = $baseURL . "6/";
-  	$Y3t4_URL = $baseURL . "10/";
-  	$Y5t6_URL = $baseURL . "8/";
-  	if ($graph == "Attendance") {
-  		$WtP_URL = $WtP_URL . "attendance/";
-  		$PtR_URL = $PtR_URL . "attendance/";
-  		$Y1t2_URL = $Y1t2_URL . "attendance/";
-  		$Y3t4_URL = $Y3t4_URL . "attendance/";
-  		$Y5t6_URL = $Y5t6_URL . "attendance/";
-
-  		$WtP_Data = extractData($WtP_URL, $api_headers);
-  		$PtR_Data = extractData($PtR_URL, $api_headers);
-  		$Y1t2_Data = extractData($Y1t2_URL, $api_headers);
-  		$Y3t4_Data = extractData($Y3t4_URL, $api_headers);
-  		$Y5t6_Data = extractData($Y5t6_URL, $api_headers);
-
-  		$WtP_Attendance = extractAttendance($WtP_Data);
-  		$PtR_Attendance = extractAttendance($PtR_Data);
-  		$Y1t2_Attendance = extractAttendance($Y1t2_Data);
-  		$Y3t4_Attendance = extractAttendance($Y3t4_Data);
-  		$Y5t6_Attendance = extractAttendance($Y5t6_Data);
-
-      $SOMETHING = array_merge($WtP_Attendance, $PtR_Attendance, $Y1t2_Attendance, $Y3t4_Attendance, $Y5t6_Attendance);
-  	}
-  } else if ($ministry == "Youth") {
-  	if ($session == "Fridays") {
-  		$YouthFridaysY7t8_URL = $baseURL . "15/";
-  		$YouthFridaysY9t10_URL = $baseURL . "16/";
-  		$YouthFridaysY11t13_URL = $baseURL . "17/";
-  	} else if ($session == "Sundays") {
-  		$YouthSundays_URL = $baseURL . "12/";
-  	}
-  } else if ($ministry == "YoungAdults") {
-  	$YA_URL = $baseURL . "";
-  }
-
-  //Define function for extracting the data from API based upon the URL formed in the if statement
-  function extractData($url, $api_headers) {
-  	$ch = curl_init($url);
-
-  	$curlopts = array();
-  	$curlopts[CURLOPT_RETURNTRANSFER] = true;
-  	$curlopts[CURLOPT_HTTPHEADER] = $api_headers;
-  	curl_setopt_array($ch, $curlopts);
-
-  	$response = curl_exec($ch);
-  	$error = curl_errno($ch);
-  	$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-
-  	$json = json_decode($response, 1);
-
-  	if (in_array($http_code, array(200, 201))) {
-  		// OK response
-  		//echo "<h1>OK: ".$http_code."</h1>";
-  		//echo "<h2>Array has " . sizeof($json) . " elements</h2>";
-  		//var_dump($json);
-  		return $json;
-  	} else {
-  		// got an error response
-  		echo '<h1>Error: '.$http_code.'</h1>';
-  		echo '<p>ChurchSuite reported the following error: '.$json['error']['message'].'</p>';
-  	}
-  }
-
-  //Define function for extracting only the total number of Kids from a session
-  function extractAttendance($dataArray){
-  	$dateArray=(array_keys($dataArray));
-
-  	$attendanceArray = array_column($dataArray, 'total');
-  	//print_r($attendanceArray);
-
-  	$attendanceData = array_combine($dateArray, $attendanceArray);
-  	//print_r($attendanceData);
-
-  	return $attendanceData;
-  }
- ?>
+?>
 
 <!DOCTYPE html>
 <html>
@@ -113,6 +15,14 @@
     <title>C3 Data Reporting | Graph </title>
     <meta name="Author" content="mia.axtell@outlook.com"/>
     <link rel="stylesheet" href="output.css">
+    <style type="text/css">
+      #chart-container {
+        width: 100%;
+        height: auto;
+      }
+    </style>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/Chart.min.js"></script>
   </head>
 
   <header class="bg-gray-800 font-montserrat">
@@ -133,31 +43,52 @@
       </ul>
     </p>
 
+    <div id="chart-container">
+      <canvas id="graphCanvas"></canvas>
+    </div>
+
     <script>
-    function generateGraph(var attendanceData) {
-      var data = <?php echo json_encode($attendanceData, JSON_NUMERIC_CHECK); ?>;
-      data = data.map(function (row, index) {
-          return {
-              x: index,
-              y: row
-          };
+      $(document).ready(function () {
+        showGraph();
       });
 
-      var chart = new CanvasJS.Chart("chartContainer", {
-          title: {
-              text: "Analysis"
-          },
-          axisY: {
-              title: "Variables"
-          },
-          data: [{
-              type: "line",
-              dataPoints: data
-          }]
-      });
-      chart.render();
+      function showGraph()
+      {
+        {
+          $.post("data.php", function (data)
+          {
+            console.log(data);
+            var name = [];
+            var marks = [];
 
-    }
+            for var i in data){
+              name.push(data[i].student_name);
+              marks.push(data[i].marks);
+            }
+
+            var chartData = {
+              labels: name,
+              datasets: [
+                {
+                  label: 'Student Marks',
+                  backgroundColor: '#49e2ff',
+                  borderColor: '#46d5f1',
+                  hoverBackgroundColor: '#CCCCCC',
+                  hoverBorderColor: '#666666',
+                  data: marks
+                }
+              ]
+            };
+            var graphTarget = $("#graphCanvas");
+
+            var barGraph = new Chart(graphTarget, {
+              type: 'bar',
+              data: chartData
+            });
+          });
+        }
+      }
     </script>
+
   </body>
 </html>
